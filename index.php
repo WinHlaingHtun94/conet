e1514beb4d9d33bbe817bc848e606d8087fdf89d
WinHlaingHtun94 <70845941+WinHlaingHtun94@users.noreply.github.com>
2021-10-29 12:04:01 +0630

    img
    img

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Connet</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="innerWrapper">
+                <div id="ctArea">
+                    <div id="ctAbout">
+                        <div class="aboutBnr">
+                            <img src="img/aboutBnr.png" >
+                        </div>
+                        <div class="sessionConsept"> 
+                            <h2 class="sessionTitle">Consept</h2>
+                            <div class="innerContent">
+                                <h2>「楽しいこと、好きなこと、わくわくすること、そんな気持ちを届けたい。」</h2>
+                                <p>このような思いから生まれたのは小さな惑星conet <br> 
+                                   私たちは阿佐ヶ谷でシルクスクリーン工房を構えています。 <br> 
+                                   まだまだ小さな星なので私たちのことを知ってもらうために <br> 暮らしや日常の中で考えたこと、感じたことなどを日々発信しています。<br>
+                                   Conetを通じて毎日が少しでも楽しくなりますようにと願いを込めて。
+                               </p>
+                            </div>
+                        </div>
+                        <div class="sessionStaff"> 
+                            <h2 class="sessionTitle">Staff</h2>
+                            <div class="innerContent">
+                                <div class="staffRow">
+                                    <div class="leftSide">
+                                        <img src="img/staff1.png">
+                                    </div>
+                                    <div class="rightSide">
+                                        <h2 class="staffName">Yoneda</h2>
+                                        <p>好きなことはたくさんありますが、特に海外旅行とカメラ、音楽を聴くこと、映画を観ることが好きです。</p>
+                                    </div>
+                                </div>
+                                 <div class="staffRow">
+                                    <div class="leftSide">
+                                        <img src="img/staff2.png">
+                                    </div>
+                                    <div class="rightSide">
+                                        <h2 class="staffName">Miyawaki</h2>
+                                        <p>植物や動物が好きです。普段はラジオを聴きながら家事をしたりしています。最近陶芸をはじめました。</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="sessionCompany"> 
+                            <h2 class="sessionTitle">Company</h2>
+                            <div class="innerContent">
+                                <div class="aboutCompany">
+                                    <dl>
+                                        <dt>会社名</dt>
+                                        <dd>株式会社 コネティス(英文名：CONNETIS Inc.)</dd>
+                                    </dl>
+                                    <dl>
+                                        <dt>所在地</dt>
+                                        <dd>〒110-0016　東京都台東区台東1-24-1 燦坤（サンクン）日本電器ビル7F</dd>
+                                    </dl>
+                                    <dl>
+                                        <dt>電話（代表）</dt>
+                                        <dd>03-6876-6389</dd>
+                                    </dl>
+                                    <dl>
+                                        <dt>資本金</dt>
+                                        <dd>600万円</dd>
+                                    </dl>
+                                    <dl>
+                                        <dt>業　種</dt>
+                                        <dd>WEB IT コンサルティングサービス、デザイン全般</dd>
+                                    </dl>
+                                    <dl>
+                                        <dt>代　表</dt>
+                                        <dd>太田　玄</dd>
+                                    </dl>
+                                    <dl>
+                                        <dt>従業員数</dt>
+                                        <dd>4名、海外スタッフ６名</dd>
+                                    </dl>
+                                    <dl>
+                                        <dt>営業開始</dt>
+                                        <dd>平成30年1月</dd>
+                                    </dl>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
